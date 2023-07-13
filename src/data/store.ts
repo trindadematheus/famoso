e788dc569baaf5a9d0176d9617329090129a1955
Matheus Trindade <trindadematheus27@gmail.com>
2023-07-13 09:59:09 -0300

    removing adlib

--- src/data/store.ts
+++ src/data/store.ts
@@ -8,7 +8,7 @@ const storeItems: Song[] = [
     value: 4,
     fee: 12,
     lyric:
-      "I wanna be a billionaire so fucking bad Buy all of the things I never had Uh, I wanna be on the cover of Forbes magazine Smiling next to Oprah and the Queen Oh, every time I close my eyes I see my name in shining lights A different city every night, oh, I, I swear The world better prepare for when I'm a billionaire",
+      "I want to be a billionaire so fucking bad Buy all of the things I never had I want to be on the cover of Forbes magazine Smiling next to Oprah and the Queen Oh every time I close my eyes I see my name in shining lights A different city every night oh I I swear The world better prepare for when I'm a billionaire",
   },
   // {
   //     videoId: 'Adb4fbt4Y0g',
